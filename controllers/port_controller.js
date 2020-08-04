5884921b9db16a68c86c7c31627b19356d09312f
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-04 16:04:57 -0700

    init setup.

---
+++ controllers/port_controller.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (sushi.js) to use its database functions.
+var sushi = require("../models/sushi.js");
+
+// Create all our routes and set up logic within those routes where required.
+
+// Loads the Home page
+router.get("/", function (req, res) {
+        
+        res.render("index", hbsObject);
+    });
+
+
+
+module.exports = router;
