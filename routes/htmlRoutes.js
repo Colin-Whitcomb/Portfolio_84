961716d53ef735bc21b787efc90e8815480791d2
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-05 09:08:59 -0700

    [function] paralax works.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -17,8 +17,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/html/index.html"));
   });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/home.html"));
   });
 
   // If no matching route is found default to home
