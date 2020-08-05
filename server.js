d5ddbac189155f50c8cdc5bf3518468917118365
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-05 07:10:19 -0700

    [edit] simple express.

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
+
+
 var express = require("express");
-
-var PORT = process.env.PORT || 8000;
-
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'))
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
 
-// // Parse request body as JSON
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// // Set Handlebars.
-// // var exphbs = require("express-handlebars");
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-// // app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set('view engine', 'html');
+// require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/port_controller.js");
+// var apiRoutes = require("./routes/apiRoutes")
+// console.log(apiRoutes); //=> [Function]
+// apiRoutes(app);
 
-// app.use(routes);
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
