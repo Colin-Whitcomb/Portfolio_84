2c2fc427ce17c92c54b08e938081a801455e2bb1
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-05 08:19:08 -0700

    [add] static method.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-
+app.use(express.static(__dirname + '/public'))
 // require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
